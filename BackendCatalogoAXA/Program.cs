1456d597f8ff8bcbce8cd50c622279661def1dc1
CARDENAS BARRIOS Johan Nicolas <johan.cardenas@axacolpatria.co>
2026-03-11 15:34:42 -0500

    feat: implementacion del metodo buscar por el i del servicio

--- BackendCatalogoAXA/Program.cs
+++ BackendCatalogoAXA/Program.cs
@@ -1,3 +1,9 @@
+using BackendCatalogoAXA.Data.Context;
+using BackendCatalogoAXA.Data.Repository.Implementation;
+using BackendCatalogoAXA.Data.Repository.Interfaces;
+using BackendCatalogoAXA.Logic.Repository.Implementation;
+using BackendCatalogoAXA.Logic.Repository.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +12,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<IServiceLogic, ServiceLogic>();
+builder.Services.AddTransient<IGetData, GetData>();
+builder.Services.AddTransient<CatalogoServiciosAxaContext, CatalogoServiciosAxaContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
