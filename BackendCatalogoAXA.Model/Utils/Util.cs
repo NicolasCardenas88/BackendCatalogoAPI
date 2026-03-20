af8faefd05c7c06748d02b6d09ed029d4fa05b2b
CARDENAS BARRIOS Johan Nicolas <johan.cardenas@axacolpatria.co>
2026-03-20 13:55:51 -0500

    fix: Se modificó metodo para que reconozca todo tipo de id y no quede reconociendo un tipo de id en especifico

--- BackendCatalogoAXA.Model/Utils/Util.cs
+++ BackendCatalogoAXA.Model/Utils/Util.cs
@@ -4,13 +4,22 @@
     {
         public static int ObtenerIdEntidad<TEntidad>(TEntidad entidad)
         {
-            var property = typeof(TEntidad).GetProperty("LogId") ?? typeof(TEntidad).GetProperty("Id");
+            var property = typeof(TEntidad)
+                .GetProperties()
+                .FirstOrDefault(p => p.Name.EndsWith("Id", StringComparison.OrdinalIgnoreCase));
+
             if (property != null)
             {
-                return (int)property.GetValue(entidad);
+                var value = property.GetValue(entidad);
+                if (value == null)
+                    throw new InvalidOperationException($"La propiedad '{property.Name}' es null en la entidad.");
+
+                return (int)value;
             }
+
             throw new InvalidOperationException("La entidad no tiene una propiedad de ID o no pudo obtenerla.");
         }
+
 
     }
 }
