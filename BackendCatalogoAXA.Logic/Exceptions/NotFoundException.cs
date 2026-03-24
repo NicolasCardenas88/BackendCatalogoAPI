0112c253839587dcfda6c491e235c1e82cdc5841
NicolasCardenas88 <johan.cardenas@axacolpatria.co>
2026-03-24 12:21:04 -0500

    Add NotFoundException class

---
+++ BackendCatalogoAXA.Logic/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+using System;
+namespace BackendCatalogoAXA.Logic.Exceptions {
+    public class NotFoundException : Exception {
+        public NotFoundException() : base("El recurso no fue encontrado") { }
+        public NotFoundException(string message) : base(message) { }
+        public NotFoundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
