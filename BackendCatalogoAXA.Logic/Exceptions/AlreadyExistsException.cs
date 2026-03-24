9957885022d9ed44235dc67ca00fe4a1ee3ceff9
NicolasCardenas88 <johan.cardenas@axacolpatria.co>
2026-03-24 12:21:20 -0500

    Add AlreadyExistsException class

---
+++ BackendCatalogoAXA.Logic/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,8 @@
+using System;
+namespace BackendCatalogoAXA.Logic.Exceptions {
+    public class AlreadyExistsException : Exception {
+        public AlreadyExistsException() : base("El recurso ya existe") { }
+        public AlreadyExistsException(string message) : base(message) { }
+        public AlreadyExistsException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
