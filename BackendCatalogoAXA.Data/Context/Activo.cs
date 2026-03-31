50c453045a0844b125ceb99200dece9c19c9f9e5
CARDENAS BARRIOS Johan Nicolas <johan.cardenas@axacolpatria.co>
2026-03-31 07:31:26 -0500

    feat: se creó una nueva tabla en la base de datos y se actualizo el EF

---
+++ BackendCatalogoAXA.Data/Context/Activo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BackendCatalogoAXA.Models;
+
+public partial class Activo
+{
+    public int ActivoId { get; set; }
+
+    public string Codigo { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public bool? TieneMfa { get; set; }
+}
