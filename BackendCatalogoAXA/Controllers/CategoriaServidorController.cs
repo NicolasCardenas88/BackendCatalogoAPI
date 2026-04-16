6327fb80d8f560e4f9f32c9d6352f5b224e71ba5
CARDENAS BARRIOS Johan Nicolas <johan.cardenas@axacolpatria.co>
2026-04-16 09:39:49 -0500

    fix:  Se arreglaron los servicios que no estaban funcionan, ya que no estaban registrando la data que se necesita y en RegisterLogic se agregaron los validator para la validación de los cambios

---
+++ BackendCatalogoAXA/Controllers/CategoriaServidorController.cs
@@ -0,0 +1,21 @@
+﻿using BackendCatalogoAXA.Logic.Repository.Interfaces;
+using BackendCatalogoAXA.Model.Dto.DtoCategoriaServidor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendCatalogoAXA.Controllers
+{
+    
+    [ApiController]
+    [Route("api/catalogo")]
+    public class CategoriaServidorController (IRegisterLogic registerLogic): ControllerBase
+    {
+        private readonly IRegisterLogic _registerLogic = registerLogic;
+
+        [HttpPost("/categoriaServidor/")]
+        public async Task<CreateCategoriaServidorDto> CreateCategoriaServidor([FromBody] CreateCategoriaServidorDto dto)
+        {
+            var result = await _registerLogic.RegisterCategoriaServidorAsync(dto);
+            return dto;
+        }
+    }
+}
--- BackendCatalogoAXA/Program.cs
+++ BackendCatalogoAXA/Program.cs
@@ -1,12 +1,13 @@
-using BackendCatalogoAXA.Model.Mapper.MapperService;
-using BackendCatalogoAXA.Model.Repository.Implementation;
-using BackendCatalogoAXA.Model.Repository.Interfaces;
+using BackendCatalogoAXA.Data.Context;
+using BackendCatalogoAXA.Data.Repository.Interfaces;
 using BackendCatalogoAXA.Logic.Repository.Implementation;
 using BackendCatalogoAXA.Logic.Repository.Interfaces;
+using BackendCatalogoAXA.Logic.Validator;
 using BackendCatalogoAXA.Middleware;
+using BackendCatalogoAXA.Model.Repository.Implementation;
+using BackendCatalogoAXA.Model.Repository.Interfaces;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
-using BackendCatalogoAXA.Data.Repository.Interfaces;
-using BackendCatalogoAXA.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,14 +19,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IServiceLogic, ServiceLogic>();
 builder.Services.AddTransient<IRegisterLogic, RegisterLogic>();
+builder.Services.AddScoped<IValidadationService, ValidationService>();
 builder.Services.AddTransient<IGetData, GetData>();
 builder.Services.AddTransient<IRegisterData, RegisterData>();
 builder.Services.AddTransient<CatalogoServiciosAxaContext, CatalogoServiciosAxaContext>();
 builder.Services.AddDbContext<CatalogoServiciosAxaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddAutoMapper(typeof(ServicioProfile));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(cfg => { }, AppDomain.CurrentDomain.GetAssemblies()); 
+builder.Services.AddValidatorsFromAssemblyContaining<ValidatorCreateFramework>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseMiddleware<ExceptionHandlingMiddleware>();
- 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
